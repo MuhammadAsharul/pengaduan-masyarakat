28eef7246466c2c16fe518a9c97cbf39e7bc5443
Yanda Agil <yandaagil@gmail.com>
2022-10-22 21:28:00 +0700

    modify navbar and home

--- resources/js/Pages/page/Home.jsx
+++ resources/js/Pages/page/Home.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import Peta from '../../Assets/peta.png'
 import illustration1 from '../../Assets/illustration1.png'
 import stepPelaporan from '../../Data/StepPelaporan'
-import ModalLapor from '../../Components/PopupLapor'
+// import ModalLapor from '../../Components/PopupLapor'
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
 
@@ -95,7 +95,6 @@ export default function Home() {
                     </div>
                     <div></div>
                   </form>
-                  {/*footer*/}
                   <div className="flex items-center justify-between p-6 border-t border-solid border-slate-200 rounded-b">
                     <div>
                     <label for="lampiran-file" className='cursor-pointer'>
@@ -134,20 +133,20 @@ export default function Home() {
             <img src={illustration1} alt="" />
           </div>
           <div className='w-3/5'>
-            <p className='font-bold text-[46px]'>Sampaikan <span className='text-[#5CE4CA]'>aspirasi</span> dan <span className='text-[#5CE4CA]'>pengaduan</span> anda</p>
-            <p className='mt-[48px] text-lg'>Pengelolaan pengaduan pelayanan publik di setiap organisasi penyelenggara di Indonesia belum terkelola secara efektif dan terintegrasi. Masing-masing organisasi penyelenggara mengelola pengaduan secara parsial dan tidak terkoordinir dengan baik. Akibatnya terjadi duplikasi penanganan pengaduan, atau bahkan bisa terjadi suatu pengaduan tidak ditangani oleh satupun organisasi penyelenggara, dengan alasan pengaduan bukan kewenangannya. Oleh karena itu, untuk mencapai visi dalam good governance maka perlu untuk mengintegrasikan sistem pengelolaan pengaduan pelayanan publik dalam satu pintu. Tujuannya, masyarakat memiliki satu saluran pengaduan secara Nasional.</p>
+            <p className='font-bold text-[#003D31] text-[46px]'>Sampaikan <span className='text-[#5CE4CA]'>aspirasi</span> dan <span className='text-[#5CE4CA]'>pengaduan</span> anda</p>
+            <p className='mt-[48px] text-[#596966] text-lg'>Pengelolaan pengaduan pelayanan publik di setiap organisasi penyelenggara di Indonesia belum terkelola secara efektif dan terintegrasi. Masing-masing organisasi penyelenggara mengelola pengaduan secara parsial dan tidak terkoordinir dengan baik. Akibatnya terjadi duplikasi penanganan pengaduan, atau bahkan bisa terjadi suatu pengaduan tidak ditangani oleh satupun organisasi penyelenggara, dengan alasan pengaduan bukan kewenangannya. Oleh karena itu, untuk mencapai visi dalam good governance maka perlu untuk mengintegrasikan sistem pengelolaan pengaduan pelayanan publik dalam satu pintu. Tujuannya, masyarakat memiliki satu saluran pengaduan secara Nasional.</p>
           </div>
         </div>
 
         <div className='flex flex-col w-4/5 min-h-screen items-center'>
-          <p className='font-bold text-[46px] mb-[122px]'>Tata Cara <span className='text-[#5CE4CA]'>Pelaporan</span></p>
+          <p className='font-bold text-[#003D31] text-[46px] mb-[122px]'>Tata Cara <span className='text-[#5CE4CA]'>Pelaporan</span></p>
           <div className='grid grid-cols-3 grid-rows-2'>
             {stepPelaporan.map((item, index) => {
               return (
                 <div key={index} className='flex flex-col justify-between p-5 m-2 gap-3  rounded-[22px] bg-[#FFFAE9]'>
                   <img src={item.image} width='55.81px' height='56px'></img>
-                  <p className='font-bold'>{item.title}</p>
-                  <p className='font-normal'>{item.sentence}</p>
+                  <p className='font-bold text-[#003D31]'>{item.title}</p>
+                  <p className='font-normal text-[#596966]'>{item.sentence}</p>
                 </div>
               )
             })}
@@ -156,7 +155,7 @@ export default function Home() {
       </div>
 
       <div className='flex flex-col items-center justify-center w-full bg-[#5CE4CA] h-[329px]'>
-        <p className='font-bold text-5xl mb-[35px]'>Jumlah Laporan Sekarang</p>
+        <p className='font-bold text-5xl text-[#003D31] mb-[35px]'>Jumlah Laporan Sekarang</p>
         <p className='font-bold text-white text-7xl'>69,420</p>
       </div>
 
