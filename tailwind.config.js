711d887c4a82a3fbb47ea6d3aee4846f6ce1352c
GhozyIkbarF <ghozyikbarf@gmail.com>
2022-10-22 09:20:19 +0700

    modify navbar and home

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Neue Montreal', ...defaultTheme.fontFamily.sans],
             },
         },
     },
